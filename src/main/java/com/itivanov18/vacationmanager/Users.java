afa96883c4c1bbadb35eeb7dffe7a1e2a0f92ffc
ITIvanov18 <ITIvanov18@codingburgas.bg>
2022-11-14 11:11:46 +0200

    Bug fix

---
+++ src/main/java/com/itivanov18/vacationmanager/Users.java
@@ -0,0 +1,69 @@
+package com.itivanov18.vacationmanager;
+
+public class Users {
+
+    private String nickname;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String team;
+
+    public Users(String nickname, String password, String firstName, String lastName, String role, String team) {
+        this.nickname = nickname;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+        this.team = team;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+}
